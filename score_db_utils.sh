bbb4a751f1c645a24351b0f140738dd0fb7f3723
Adam.Schneider <Adam.Schneider@noaa.gov>
2024-01-31 00:24:16 +0000

    Removed modules installed on /contrib from utils script and registered scout run 1979 stream for testing

--- score_db_utils.sh
+++ score_db_utils.sh
@@ -1,10 +1,5 @@
 #!/bin/bash --posix
 
-module use /contrib/home/builder/UFS-RNR-stack/modules
-module load anaconda3
-echo 'modules loaded'
-which python3
-
 SCORE_DB_HOME_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export PYTHONPATH=$SCORE_DB_HOME_DIR/src
 export PYTHONPATH=$SCORE_DB_HOME_DIR/../score-hv/src
