d1c66eca805e927d5ceea128f593aac050405bc4
Jessica Knezha <jessica.knezha@noaa.gov>
2024-03-26 16:05:12 -0600

    sat meta tests

---
+++ tests/test_sat_meta_handler.py
@@ -0,0 +1,52 @@
+"""
+Copyright 2024 NOAA
+All rights reserved.
+
+Unit tests for sat_meta.py
+
+"""
+import os
+import pathlib
+import pytest
+import json
+from collections import namedtuple
+
+from sat_meta import SatMetaRequest
+
+
+def test_sat_meta_input_dict():
+    request_dict = {
+        'name': 'sat_meta',
+        'method' : 'PUT',
+        'body' :{
+            'name': 'example_sat_meta',
+            'sat_id': '123456789',
+            'sat_name': 'Example Sat Name',
+            'sensor': 'examplesensor',
+            'channel': '1',
+            'scan_angle':'90 degreees sectors fore- and aft-'
+        }
+    }
+
+    smr = SatMetaRequest(request_dict)
+    result = smr.submit()
+    print(f'Sat Meta PUT results: {result}')
+    assert(result.success)
+
+def test_sat_meta_get_request():
+    request_dict = {
+        'name': 'sat_meta',
+        'method': 'GET',
+        'params' : {
+            'filters': {
+                'name' :{
+                    'exact' : 'example_sat_meta'
+                }
+            }
+        }
+    }
+
+    smr = SatMetaRequest(request_dict)
+    result = smr.submit()
+    print(f'Sat Meta GET results: {result}')
+    assert(result.success)
