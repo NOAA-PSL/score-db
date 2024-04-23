93d8583c4a5a1ebbf870a14893af1f459375bb71
Jessica Knezha <jessica.knezha@noaa.gov>
2024-04-23 12:16:53 -0600

    inital test cases for instrument meta handler

---
+++ tests/test_instrument_meta_handler.py
@@ -0,0 +1,44 @@
+"""
+Copyright 2024 NOAA
+All rights reserved.
+
+Unit tests for instrument_meta.py
+
+"""
+
+from instrument_meta import InstrumentMetaRequest
+
+def test_instrument_meta_input_dict():
+    request_dict = {
+        'name': 'instrument_meta',
+        'method' : 'PUT',
+        'body' : {
+            'name': 'example_instrument',
+            'num_channels': 34,
+            'scan_angle': 'The example of scan angle'
+        }
+    }
+
+    imr = InstrumentMetaRequest(request_dict)
+    result = imr.submit
+    print(f'Instrument Meta PUT results: {result}')
+    assert(result.success)
+
+def test_instrument_meta_get_request():
+    request_dict = {
+        'name': 'instrument_meta',
+        'method': 'GET',
+        'params' : {
+            'filters' : {
+                'name' : {
+                    'exact' : 'example_instrument'
+                }
+            }
+        }
+    }
+
+    imr = InstrumentMetaRequest(request_dict)
+    result = imr.submit
+    print(f'Instrument Meta GET results: {result}')
+    assert(result.success)
+    assert(result.details.get('record_count') > 0)
