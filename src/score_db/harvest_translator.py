4c7e9fbd8b22d5981ea21a010b9b911c1fa0e752
Kevin <kevin.counts@noaa.gov>
2024-06-06 16:17:06 -0600

    Retrack all of the files moved inside of the new score_db directory level

---
+++ src/score_db/harvest_translator.py
@@ -0,0 +1,97 @@
+"""
+Copyright 2023 NOAA
+All rights reserved.
+
+Collection of methods to translate results from harvesters
+into input data relevant for storage in the columns defined in
+the db table models.
+"""
+
+from collections import namedtuple
+
+#data structure for what is stored in the database, corresponds to the db columns
+#corresponds to singular input metric values 
+MetricTableData = namedtuple(
+    'MetricTableData',
+    [
+        'name',
+        'region_name',
+        'elevation',
+        'elevation_unit',
+        'value',
+        'cycletime',
+        'forecast_hour',
+        'ensemble_member'
+    ],
+)
+
+#corresponds to array structured metric values 
+ArrayMetricTableData = namedtuple(
+    'ArrayMetricTableData',
+    [
+        'name',
+        'region_name',
+        'value',
+        'assimilated',
+        'time_valid',
+        'forecast_hour',
+        'ensemble_member',
+        'sat_meta_name',
+        'sat_id',
+        'sat_name',
+        'sat_short_name',
+    ],
+) 
+
+def inc_logs_translator(harvested_data):
+    """ Expected output from inc_logs harvester 
+    inc_logs_harvested_data = namedtuple(
+        'HarvestedData',
+        [
+            'logfile',
+            'cycletime',
+            'statistic',
+            'variable',
+            'value',
+            'units'
+        ]
+    )
+    """
+    result = MetricTableData(
+        harvested_data.statistic + "_" + harvested_data.variable,
+        'global',
+        None,
+        'N/A',
+        harvested_data.value,
+        harvested_data.cycletime,
+        None,
+        None)
+    return result
+
+def daily_bfg_translator(harvested_data):
+    """ Expected output from daily bfg harvester
+    daily_bfg_harvested_data = namedtuple(
+        'HarvestedData', 
+        [
+            'filenames',
+            'statistic',
+            'variable',
+            'value',
+            'units',
+            'mediantime',
+            'longname'
+        ]
+    )
+    """
+    result = MetricTableData(
+        harvested_data.statistic + "_" + harvested_data.variable,
+        'global',
+        None,
+        'N/A',
+        harvested_data.value,
+        harvested_data.mediantime,
+        None,
+        None
+    )        
+    
+    return result
