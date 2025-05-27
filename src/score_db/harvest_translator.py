d75fc07655c2481b52e5fae3a0d7ca7358753b4b
Adam Schneider <adam.schneider@noaa.gov>
2025-05-27 17:23:01 -0600

    modification of the daily_bfg harvest translator, so that it accounts for background (_fg) versus analysis (_an) in metric name

--- src/score_db/harvest_translator.py
+++ src/score_db/harvest_translator.py
@@ -98,6 +98,7 @@ def daily_bfg_translator(harvested_data):
         'HarvestedData', 
         [
             'filenames',
+            'segment',
             'statistic',
             'variable',
             'value',
@@ -107,8 +108,16 @@ def daily_bfg_translator(harvested_data):
         ]
     )
     """
+
+    metric_name = harvested_data.statistic + "_" + harvested_data.variable,
+    
+    if harvested_data.segment == 'background':
+        metric_name += '_fg'
+    elif harvested_data.segment == 'analysis':
+        metric_name += '_an'
+    
     result = MetricTableData(
-        harvested_data.statistic + "_" + harvested_data.variable,
+        metric_name,
         'global',
         None,
         None,
